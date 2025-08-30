aa0444cb9b20049b6eb5d97b33b75c0345e63ae2
ryanuo <1577791638@qq.com>
2025-08-30 13:44:33 +0800

    feat(DevicePreview): add loading state for screenshot capture
    - Add loading state to indicate screenshot process
    - Display overlay with loading animation during screenshot capture
    - Improve user experience by providing visual feedback

--- app/components/DevicePreview.vue
+++ app/components/DevicePreview.vue
@@ -83,6 +83,7 @@ const protocol = ref('https')
 const url = ref('www.ryanuo.cc')
 const scrollBar = ref(false)
 const coverShow = ref(false)
+const loading = ref(false)
 
 function resetPreview() {
   activeDevice.value = ''
@@ -96,6 +97,7 @@ function handleActiveDevice(device: Device) {
 }
 
 async function fetchScreenshot() {
+  loading.value = true
   try {
     // 获取当前预览的 url
     let targetUrl = ''
@@ -130,6 +132,9 @@ async function fetchScreenshot() {
   }
   catch (err) {
     console.error('截图失败', err)
+  }
+  finally {
+    loading.value = false
   }
 }
 
@@ -196,6 +201,16 @@ applyQueryParams()
 
 <template>
   <section class="mx-auto p-t-30 relative">
+    <!-- 截图 loading 蒙层 -->
+    <transition name="fade">
+      <div v-if="loading" class="bg-black/40 flex items-center inset-0 justify-center fixed z-[99999]">
+        <div class="flex flex-col items-center">
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="none" stroke="#fff" stroke-linecap="round" stroke-width="2" d="M12 6.99998C9.1747 6.99987 6.99997 9.24998 7 12C7.00003 14.55 9.02119 17 12 17C14.7712 17 17 14.75 17 12"><animateTransform attributeName="transform" attributeType="XML" dur="560ms" from="0,12,12" repeatCount="indefinite" to="360,12,12" type="rotate" /></path></svg>
+          <span class="text-lg text-white">正在截图，请稍候...</span>
+        </div>
+      </div>
+    </transition>
+
     <!-- 覆盖层 -->
     <transition name="fade">
       <div v-if="coverShow" class="p-10 bg-white flex inset-0 justify-end fixed z-100">
